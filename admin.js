b23b32bfadeb1c37544a2245e33caf403d144939
Yogesh Adhikari <adhikariprema29@gmail.com>
2026-03-05 07:01:25 +0530

    sata andagi

--- admin.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcrypt');
-const pass = '0000';
-const passGenerator = async (pass)=>{
-    passEncrypted = await bcrypt.hash(pass,10);
-    console.log(passEncrypted);
-}
-passGenerator(pass);
